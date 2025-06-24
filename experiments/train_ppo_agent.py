c7454b72ef26e3aee1b9258421e7f0c9033fc714
tidiane-camaret <camaret.tidiane@gmail.com>
2025-06-24 12:33:19 +0200

    Update dependencies and clean up project structure
    - Modified `.gitignore` to include all egg-info and results directories.
    - Added `ipykernel` and `jupyter` as dependencies in `pyproject.toml`.
    - Updated `uv.lock` to reflect new package versions and added several new packages.
    - Removed the obsolete `eval_ppo_agent.py` file.
    - Enhanced `image_analysis.ipynb` with updated transformations and model loading logic.

--- experiments/train_ppo_agent.py
+++ experiments/train_ppo_agent.py
@@ -1,38 +1,37 @@
 import numpy as np
 
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CheckpointCallback
 import sys
 sys.path.append("/work/dlclarge2/ndirt-SegFM3D/learnedpatch")  # Adjust the path as necessary to import medsegbench
 
 from src.patchselectionenv import GymPatchSelectionEnv, dice_score
 
-
-"""
-# Dummy data
-image = np.zeros((256, 256))
-mask = np.zeros((256, 256))
-mask[50:150, 50:150] = 1
-
-"""
-
 from medsegbench import Promise12MSBench
 import torch.utils.data as data
-import torchvision.transforms as transforms
+import torch
+import torchvision.transforms.v2 as transforms
 
 data_transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale=True),
+    #transforms.RandomCrop(256)
 ])
-val_dataset = Promise12MSBench(split="val", transform=data_transform, target_transform=data_transform, download=True)
-image_list = [val_dataset[i][0] for i in range(len(val_dataset))]
-mask_list = [val_dataset[i][1] for i in range(len(val_dataset))]
-patch_size = (64, 64)
+
+crop_transform = transforms.RandomCrop(280)
+train_dataset = Promise12MSBench(split="train", transform=data_transform, target_transform=data_transform, download=True)
+
+image_list = [train_dataset[i][0] for i in range(len(train_dataset))]
+mask_list = [train_dataset[i][1] for i in range(len(train_dataset))]
+patch_size = (32, 32)
 max_steps = 10
 
 env = GymPatchSelectionEnv(image_list, mask_list, patch_size, max_steps)
 
+checkpoint_callback = CheckpointCallback(save_freq=1000, save_path="results/ppo_learnedpatch")  
 # Train PPO agent
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10)
+model.learn(total_timesteps=20000, callback=checkpoint_callback)
 
 # Evaluate
 obs = env.reset()
@@ -44,3 +43,5 @@ while not done:
 
 final_dice = dice_score(env.env.current_mask, env.env.mask)
 print(f"Final Dice score: {final_dice:.4f}")
+
+#model.save("results/ppo_learnedpatch.pkl")
